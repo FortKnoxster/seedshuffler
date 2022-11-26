d374dd82ff905340899bb6c58d1b84851c3b02da
mickey <mj@fortknoxster.com>
2022-11-26 15:07:28 +0100

    Added variables/constants

--- src/constants/variables.js
+++ src/constants/variables.js
@@ -1,3 +1,10 @@
+// App details
+export const APP_NAME = process.env.REACT_APP_NAME
+export const APP_BRAND = process.env.REACT_APP_BRAND
+export const APP_BRAND_NAME = process.env.REACT_APP_BRAND_NAME
+export const DEFAULT_LOCALE = process.env.REACT_APP_DEFAULT_LOCALE
+
+// Wordlists
 export const WL_CHINESE_SI = 'chinese_simplified'
 export const WL_CHINESE_TR = 'chinese_traditional'
 export const WL_CZECH = 'czech'
@@ -8,4 +15,15 @@ export const WL_JAPANESE = 'japanese'
 export const WL_KOREAN = 'korean'
 export const WL_PORTUGUESE = 'portuguese'
 export const WL_SPANISH = 'spanish'
-export const DEFAULT_LOCALE = process.env.REACT_APP_DEFAULT_LOCALE
+
+// Wordlist languages
+export const WL_CHINESE_SI_NAME = '中文(简体)'
+export const WL_CHINESE_TR_NAME = '中文(繁體)'
+export const WL_CZECH_NAME = 'Čeština'
+export const WL_ENGLISH_NAME = 'English'
+export const WL_FRENCH_NAME = 'Français'
+export const WL_ITALIAN_NAME = 'Italiano'
+export const WL_JAPANESE_NAME = '日本語 '
+export const WL_KOREAN_NAME = '한국어'
+export const WL_PORTUGUESE_NAME = 'Português'
+export const WL_SPANISH_NAME = 'Español'
