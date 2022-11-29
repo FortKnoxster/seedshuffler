4fee798bb52a921c5e95feff8654785272a4fd42
mickey <mj@fortknoxster.com>
2022-11-29 22:44:19 +0100

    New content component (husky test)

---
+++ src/components/MenuContent.js
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled/macro'
+
+const MenuContent = ({ children, isVisible }) => {
+  return <Container className={isVisible ? 'open' : ''}>{children}</Container>
+}
+console.log('hihi')
+export default MenuContent
+
+const Container = styled.div({
+  display: 'none',
+  flexDirection: 'column',
+  height: '100%',
+  position: 'relative',
+  '&.open': {
+    display: 'block',
+    //transition: 'all 12.5s ease-in-out',
+    //height: '100%',
+  },
+})
