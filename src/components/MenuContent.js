8863ef6c07878be3719ea31e5782841c84d6326a
mickey <mj@fortknoxster.com>
2022-11-29 23:53:46 +0100

    husky pre-commit test

--- src/components/MenuContent.js
+++ src/components/MenuContent.js
@@ -4,7 +4,6 @@ const MenuContent = ({ children, isVisible }) => {
   return <Container className={isVisible ? 'open' : ''}>{children}</Container>
 }
 console.log('hihi')
-
 export default MenuContent
 
 const Container = styled.div({
